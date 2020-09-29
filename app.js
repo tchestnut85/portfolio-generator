c1cf049f9727274b0b6644b8a05d1fd09f2b78a7
Tom Chestnut <tchestnut85@gmail.com>
2020-09-28 21:19:23 -0400

    Modularized js files to have a seperate file for HTML page template function.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
