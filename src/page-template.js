f6e308f345ca40bfee047aac779ed6ddf9ea6b10
Tom Chestnut <tchestnut85@gmail.com>
2020-10-07 17:52:30 -0400

    fixed bugs to get working app

--- src/page-template.js
+++ src/page-template.js
@@ -28,7 +28,7 @@ const generateProjects = projectsArr => {
               ${languages.join(', ')}
             </h5>
             <p>${description}</p>
-            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            <a href="${link}" target='_blank' class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `;
             })
@@ -45,7 +45,7 @@ const generateProjects = projectsArr => {
               ${languages.join(', ')}
             </h5>
             <p>${description}</p>
-            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            <a href="${link}" target='_blank' class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `;
             })
@@ -79,12 +79,13 @@ module.exports = templateData => {
                     <div class="container flex-row justify-space-between align-center py-3">
                         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
                         <nav class="flex-row">
-                            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
+                            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}" target='_blank'>GitHub</a>
                         </nav>
                     </div>
                     </header>
                 <main class="container my-5">
                     ${generateAbout(about)}
+                    ${generateProjects(projects)}
                 </main>
                 <footer class="container text-center py-3">
                     <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
--- utils/generate-site.js
+++ utils/generate-site.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
         // added this next line
-        fs.writeFile('.dist/index.html', fileContent, err => {
-        // if theres an error, reject the Promise and send the error to the Promise'ss .catch method
-        if (err) {
-            reject(err);
-            return;
-        }
-        // if everything went well, resolve the Promise into the .then method
-        resolve({
-            ok: true,
-            message: 'File created!'
-        });
-        // added next brackets
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            // if theres an error, reject the Promise and send the error to the Promise'ss .catch method
+            if (err) {
+                reject(err);
+                return;
+            }
+            // if everything went well, resolve the Promise into the .then method
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+            // added next brackets
         });
     });
 };
@@ -22,14 +22,14 @@ const writeFile = fileContent => {
 const copyFile = () => {
     return new Promise((resolve, reject) => {
         fs.copyFile('./src/style.css', './dist/style.css', err => {
-        if (err) {
-            reject(err);
-            return;
-        }
-        resolve({
-            ok: true,
-            message: 'File copied!'
-        });
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File copied!'
+            });
         });
     });
 };
